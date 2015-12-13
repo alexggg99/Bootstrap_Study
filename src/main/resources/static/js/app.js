cacf4b56935bb277c1df3dd3107d8df7d75f4255
Alex <alexggg99@gmail.com>
2015-12-13 21:51:54 +0300

    ecommerce_app (init push)

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,24 +1,6 @@
 ///**
 // * Created by alexggg99 on 03.07.15.
 // */
-////(function() {
-//    //var app = angular.module('myApp', []);
-//    var app=angular.module("myApp",[]);
-//
-//    app.config(["$routeProvider",function($routeProvider){
-//        $routeProvide.when('/', {
-//            templateUrl:"home.html",
-//            controller:'HomeController'
-//        })
-//            .otherwise({redirectTo:'/'})
-//    }])
-////})
-//
-//function HomeController(){
-//
-//}
-//
-//app.controller('HomeController', HomeController);
 
 var app = angular.module('myApp', ['ngRoute']);
 
